b9c19627c653cbc7478087e920974c55fe347826
Jovit-Mathew236 <jovitmathew236@gmail.com>
2024-10-09 11:28:26 +0530

    small update  view port

--- app/layout.tsx
+++ app/layout.tsx
@@ -100,6 +100,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+
         <link rel="apple-touch-startup-image" href="/splash.png" />
         {/* Add multiple splash screen images for different iOS device sizes */}
         <link
