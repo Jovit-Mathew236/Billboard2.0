b3f83e133862135d7f7f34b7581b16caf91345af
Jovit-Mathew236 <jovitmathew236@gmail.com>
2024-10-09 16:23:43 +0530

    bug fix

--- lib/updateZuztand.tsx
+++ lib/updateZuztand.tsx
@@ -1,35 +1,35 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import { useUserStore } from "@/lib/store/useUserStore";
-import { profileDataType } from "@/components/madeups/profile";
-import { ProfileData } from "./dasboard";
+// "use client";
+// import  { useEffect, useState } from "react";
+// import { useUserStore } from "@/lib/store/useUserStore";
+// // import { profileDataType } from "@/components/madeups/profile";
+// // import { ProfileData } from "./dasboard";
 
-const UpdateZustand = () => {
-  const { setUserName, setUserImageUrl } = useUserStore();
-  const [profileData, setProfileData] = useState<profileDataType | null>(null);
+// const UpdateZustand = () => {
+//   const { setUserName, setUserImageUrl } = useUserStore();
+//   // const [profileData, setProfileData] = useState<profileDataType | null>(null);
 
-  useEffect(() => {
-    const fetchProfileData = async () => {
-      try {
-        const data = await ProfileData();
-        setProfileData(data);
-      } catch (error) {
-        console.error(error);
-        // Handle error
-      }
-    };
+//   useEffect(() => {
+//     const fetchProfileData = async () => {
+//       try {
+//         const data = await ProfileData();
+//         // setProfileData(data);
+//       } catch (error) {
+//         console.error(error);
+//         // Handle error
+//       }
+//     };
 
-    fetchProfileData();
-  }, []); // Empty dependency array to run the effect only once
+//     fetchProfileData();
+//   }, []); // Empty dependency array to run the effect only once
 
-  useEffect(() => {
-    if (profileData) {
-      setUserName(profileData.data?.name ?? "");
-      setUserImageUrl(profileData.data?.image_url ?? "");
-    }
-  }, [profileData, setUserName, setUserImageUrl]);
+//   useEffect(() => {
+//     if (profileData) {
+//       setUserName(profileData.data?.name ?? "");
+//       setUserImageUrl(profileData.data?.image_url ?? "");
+//     }
+//   }, [profileData, setUserName, setUserImageUrl]);
 
-  return null; // This component doesn't render anything
-};
+//   return null; // This component doesn't render anything
+// };
 
-export default UpdateZustand;
+// export default UpdateZustand;
