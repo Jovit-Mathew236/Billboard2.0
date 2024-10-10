9fee7ac98f3431ef5cfa591eabf64e6effcb7a9a
Jovit-Mathew236 <jovitmathew236@gmail.com>
2024-10-10 19:59:42 +0530

    updated manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -5,8 +5,10 @@ export default function manifest(): MetadataRoute.Manifest {
     name: "Billboard",
     short_name: "Billboard",
     description: "App for managing real-time billboards",
+    scope: "/",
     start_url: "/",
-    display: "standalone",
+    display: "minimal-ui",
+    orientation: "portrait",
     background_color: "#f0f0f0", // A light gray for a neutral splash screen
     theme_color: "#ff5722", // A vibrant color for the status bar
     icons: [
