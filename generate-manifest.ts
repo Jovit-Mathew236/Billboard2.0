6313fa512ac396e4dacc54265d1063133eec1d09
Jovit-Mathew236 <jovitmathew236@gmail.com>
2024-10-11 12:17:52 +0530

    fix

--- generate-manifest.ts
+++ generate-manifest.ts
@@ -10,5 +10,6 @@ const manifestJson = JSON.stringify(manifestObj, null, 2);
 
 // Write to public directory
 writeFileSync(join(process.cwd(), "public", "manifest.json"), manifestJson);
+writeFileSync(join(process.cwd(), "public", "site.webmanifest"), manifestJson);
 
 console.log("manifest.json generated successfully");
