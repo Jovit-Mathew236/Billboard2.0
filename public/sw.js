a953fb2fd6d4e3ccd4e3f69be6e1b5d2d78118f2
Jovit-Mathew236 <jovitmathew236@gmail.com>
2024-10-11 12:13:26 +0530

    feat: mainfest edited

--- public/sw.js
+++ public/sw.js
@@ -3,8 +3,8 @@ self.addEventListener("push", function (event) {
     const data = event.data.json();
     const options = {
       body: data.body,
-      icon: data.icon || "/android-chrome-96x96.png",
-      badge: "/android-chrome-96x96.png",
+      icon: data.icon || "/android/android-launchericon-96-96.png",
+      badge: "/android/android-launchericon-96-96.png",
       vibrate: [100, 50, 100],
       data: {
         dateOfArrival: Date.now(),
@@ -19,4 +19,56 @@ self.addEventListener("notificationclick", function (event) {
   console.log("Notification click received.");
   event.notification.close();
   event.waitUntil(clients.openWindow("<https://google.com>"));
+});
+
+// This is the "Offline page" service worker
+
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
+);
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener("install", async (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      (async () => {
+        try {
+          const preloadResp = await event.preloadResponse;
+
+          if (preloadResp) {
+            return preloadResp;
+          }
+
+          const networkResp = await fetch(event.request);
+          return networkResp;
+        } catch (error) {
+          console.log("Fetch failed; returning offline page", error);
+
+          const cache = await caches.open(CACHE);
+          const cachedResp = await cache.match(offlineFallbackPage);
+          return cachedResp;
+        }
+      })()
+    );
+  }
 });
