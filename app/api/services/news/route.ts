710aae7a835077cf81be2011a11935770fb93390
Jovit-Mathew236 <jovitmathew236@gmail.com>
2025-02-14 00:02:03 +0530

    chore: Temporarily disable external API calls in news and NIFTY routes

--- app/api/services/news/route.ts
+++ app/api/services/news/route.ts
@@ -2,18 +2,19 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const response = await fetch(
-      "https://api.thenewsapi.com/v1/news/top?api_token=d5wP5LsdfHb662Krn2hufBpzWNHTXNJAJsqxPjxp&locale=in&limit=3&categories=business,sports,tech,general&exclude_domains=dnaindia.com",
-      {
-        next: { revalidate: 300 }, // Cache for 5 minutes
-      }
-    );
+    // const response = await fetch(
+    //   "https://api.thenewsapi.com/v1/news/top?api_token=d5wP5LsdfHb662Krn2hufBpzWNHTXNJAJsqxPjxp&locale=in&limit=3&categories=business,sports,tech,general&exclude_domains=dnaindia.com",
+    //   {
+    //     next: { revalidate: 300 }, // Cache for 5 minutes
+    //   }
+    // );
 
-    if (!response.ok) {
-      throw new Error("Failed to fetch news data");
-    }
+    // if (!response.ok) {
+    //   throw new Error("Failed to fetch news data");
+    // }
 
-    const data = await response.json();
+    // const data = await response.json();
+    const data = {};
     return NextResponse.json(data);
   } catch (error) {
     console.error("Error fetching news data:", error);
